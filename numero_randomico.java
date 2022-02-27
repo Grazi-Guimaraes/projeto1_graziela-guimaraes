b052fe188218f0bdd4c34c1042f92632bf8b019e
Graziela Guimarães <grazielaguimaraes1988@gmail.com>
2022-02-26 23:09:12 -0300

    commit inicial

---
+++ numero_randomico.java
@@ -0,0 +1,57 @@
+package exercicios_logica;
+
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Graziela
+ */
+public class Exercicios_logica {
+
+    public static void main(String[] args) {
+
+/******************************************/
+        //Cria um objeto random
+        Random randomico = new Random();
+
+        //Até 10
+        int recebeRandomico = randomico.nextInt(10);
+
+        //Mostra o número sorteado
+        System.out.println("Número sorteado: " + recebeRandomico);
+        
+        
+        //Chances de jogar (vidas)
+        int chances  = 5;
+        
+        //enquanto
+        while(chances > 0) {
+            
+        //Recebe seu palpite via caixa de entrada
+        int palpite = Integer.parseInt(JOptionPane.showInputDialog("Insira um número de 0 até 10"));
+
+        if(recebeRandomico == palpite){
+            JOptionPane.showMessageDialog(null, "Acertou! Número escolhido: " + palpite);
+            //Fecha o sistema
+            System.exit(0);
+            
+        } else if(recebeRandomico < palpite){
+            
+             //retira uma chance
+             chances--;
+             JOptionPane.showMessageDialog(null, "Errou! Número escolhido: " + palpite + ", foi maior que o número sorteado, suas chances: " + chances);
+
+        } else{
+            
+             //retira uma chance
+             chances--;
+             JOptionPane.showMessageDialog(null, "Errou! Número escolhido: " + palpite + ", foi menor que o número sorteado, suas chances: " + chances);
+            
+        }
+        
+       }//fim enquanto
+
+        
+    }
+}
