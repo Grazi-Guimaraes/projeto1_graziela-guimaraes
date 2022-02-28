9153d98c98402a25608473271f20d735c2342587
Graziela Guimarães <grazielaguimaraes1988@gmail.com>
2022-02-28 20:50:52 -0300

    Copy Value Of

--- copy_value_of.java
+++ copy_value_of.java
@@ -16,9 +16,18 @@ public class Controlador {
         }
 
         //Juntando todas as letras do array
+        System.out.println("/*******************************/");
         String palavraCompleta = String.copyValueOf(soletrando);
         System.out.println("Palavra completa: " + palavraCompleta);
+        
+        //Indica onde iremos iniciar o array e quantas letras iremos contar após iniciarmos.
+        System.out.println("/*******************************/");
+        String off = String.copyValueOf(soletrando, 2, 2);
+        System.out.println("Letras que foram selecionadas: " + off);
 
     }
 
-}
+}  
+   
+    
+
