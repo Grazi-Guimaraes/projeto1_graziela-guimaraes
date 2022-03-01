07fcaf35e6f2863b6a82ed13fdb258ecb0dc2bf5
Graziela Guimarães <grazielaguimaraes1988@gmail.com>
2022-02-28 23:47:54 -0300

    Quanto devo beber de água por dia?

---
+++ quanto_beber_agua.java
@@ -0,0 +1,19 @@
+﻿package controller;
+
+/**
+ *
+ * @author Graziela Guimarães
+ */
+public class Controlador {
+
+    public static void main(String[] args) {
+        
+        
+        //Quanto devo beber de água por dia?
+        Double peso = 65.00; //Seu peso
+        Double litrosAgua = peso * 35; //35ml para cada kilo
+        System.out.println("Devo beber " + litrosAgua);
+
+  }
+
+}
